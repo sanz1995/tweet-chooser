c6c309a6158043344ce9e07564416feb6781944a
Jorge Sanz <jorge.sanz.alcaine@gmail.com>
2018-03-29 11:09:07 +0200

    cliente streaming

---
+++ src/main/java/chooser/TweetListener.java
@@ -0,0 +1,61 @@
+package chooser;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.social.twitter.api.StreamDeleteEvent;
+import org.springframework.social.twitter.api.StreamListener;
+import org.springframework.social.twitter.api.StreamWarningEvent;
+import org.springframework.social.twitter.api.Tweet;
+
+/**
+ * Created by jorge on 29/03/18.
+ */
+public class TweetListener implements StreamListener {
+
+    private RabbitTemplate r;
+
+    public TweetListener(RabbitTemplate rabbit){
+        r = rabbit;
+
+        r.convertAndSend("tweet","hola");
+    }
+
+
+    @Override
+    public void onTweet(Tweet tweet) {
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+
+        try {
+            String json = ow.writeValueAsString((Object)tweet);
+
+            System.out.println("Enviando...");
+            r.convertAndSend("tweet",json);
+            System.out.println("Enviado");
+
+
+        } catch (JsonProcessingException e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onDelete(StreamDeleteEvent deleteEvent) {
+
+    }
+
+    @Override
+    public void onLimit(int numberOfLimitedTweets) {
+
+    }
+
+    @Override
+    public void onWarning(StreamWarningEvent warningEvent) {
+
+    }
+}
